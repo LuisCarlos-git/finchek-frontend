0522ed4016831e41d0d68f8f7d6fbe3ec8f415da
LuisCarlos-git <luisestrela.bol951@gmail.com>
2023-09-02 20:02:48 -0300

    feat: add auth guard for private routes

---
+++ src/router/guards/AuthGuard.tsx
@@ -0,0 +1,19 @@
+import { RoutePaths } from '@/types/Routes';
+import { Navigate, Outlet } from 'react-router-dom';
+
+interface AuthGuardProps {
+  isPrivate?: boolean;
+}
+
+export const AuthGuard = ({ isPrivate = false }: AuthGuardProps) => {
+  const signedIn = false;
+
+  if (!signedIn && isPrivate) {
+    return <Navigate to={RoutePaths.LOGIN} replace />;
+  }
+  if (signedIn && !isPrivate) {
+    return <Navigate to={RoutePaths.DASHBOARD} replace />;
+  }
+
+  return <Outlet />;
+};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,18 @@
-import { RoutePaths } from '@/types/Routes';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { RoutePaths } from '@/types/Routes';
+import { AuthGuard } from './guards/AuthGuard';
 
 export const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path={RoutePaths.LOGIN} element={<h1>login</h1>} />
-      <Route path={RoutePaths.REGISTER} element={<h1>register</h1>} />
-      <Route path={RoutePaths.DASHBOARD} element={<h1>dash</h1>} />
+      <Route element={<AuthGuard />}>
+        <Route path={RoutePaths.LOGIN} element={<h1>login</h1>} />
+        <Route path={RoutePaths.REGISTER} element={<h1>register</h1>} />
+      </Route>
+      <Route element={<AuthGuard isPrivate />}>
+        <Route path={RoutePaths.DASHBOARD} element={<h1>dashboard</h1>} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
