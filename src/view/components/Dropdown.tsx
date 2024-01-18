389beeafa6b97a2f72bd92c7bc4d22340d67dfd3
Luis Carlos [FRONT] <luisestrela.bol951@gmail.com>
2024-01-17 23:03:42 -0300

    feat: add user dropdown

--- src/view/components/Dropdown.tsx
+++ src/view/components/Dropdown.tsx
@@ -1,8 +1,19 @@
-import * as RdxDropdown from '@radix-ui/react-dropdown-menu';
 import React from 'react';
+import * as RdxDropdown from '@radix-ui/react-dropdown-menu';
+
+import { cn } from '@/app/utils/cn';
 
 interface ComponentWithChildren {
   children: React.ReactNode;
+}
+
+interface ItemProps extends ComponentWithChildren {
+  className?: string;
+  onSelect?: () => void;
+}
+
+interface ContentProps extends ComponentWithChildren {
+  className?: string;
 }
 
 function Root({ children }: ComponentWithChildren) {
@@ -17,18 +28,30 @@ function Trigger({ children }: ComponentWithChildren) {
   );
 }
 
-function Item({ children }: ComponentWithChildren) {
+function Item({ children, className, onSelect }: ItemProps) {
   return (
-    <RdxDropdown.Item className="min-h-[48px] outline-none flex items-center p-2 text-sm text-gray-800 hover:bg-gray-50">
+    <RdxDropdown.Item
+      onSelect={onSelect}
+      className={cn(
+        ' cursor-pointer min-h-[48px] outline-none flex items-center py-2 px-4 text-sm text-gray-800transition-colors rounded-2xl data-[highlighted]:bg-gray-50',
+        className
+      )}
+    >
       {children}
     </RdxDropdown.Item>
   );
 }
 
-function Content({ children }: ComponentWithChildren) {
+function Content({ children, className }: ContentProps) {
   return (
     <RdxDropdown.Portal>
-      <RdxDropdown.Content className="rounded-2xl p-2 bg-white space-y-2 shadow-md">
+      <RdxDropdown.Content
+        sideOffset={4}
+        className={cn(
+          'rounded-2xl p-2 bg-white space-y-2 shadow-md data-[side=top]:animate-slide-up-and-fade data-[side=bottom]:animate-slide-down-and-fade',
+          className
+        )}
+      >
         {children}
       </RdxDropdown.Content>
     </RdxDropdown.Portal>
--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,16 +1,27 @@
+import { ExitIcon } from '@radix-ui/react-icons';
 import { Dropdown } from './Dropdown';
+import { useAuth } from '@/app/hooks/context/useAuth';
 
-// className="w-12 h-12 rounded-full flex items-center justify-center bg-teal-0"
-
-export const UserMenu = () => (
-  <Dropdown.Root>
-    <Dropdown.Trigger>
-      <span className="text-sm text-teal-900 font-medium tracking-[-0.5px]">
-        LC
-      </span>
-    </Dropdown.Trigger>
-    <Dropdown.Content>
-      <Dropdown.Item>sair</Dropdown.Item>
-    </Dropdown.Content>
-  </Dropdown.Root>
-);
+export const UserMenu = () => {
+  const { signOut } = useAuth();
+  return (
+    <Dropdown.Root>
+      <Dropdown.Trigger>
+        <div className="w-12 h-12 rounded-full flex items-center justify-center bg-teal-0">
+          <span className="text-sm text-teal-900 font-medium tracking-[-0.5px]">
+            LC
+          </span>
+        </div>
+      </Dropdown.Trigger>
+      <Dropdown.Content className="w-48">
+        <Dropdown.Item
+          onSelect={signOut}
+          className="flex items-center justify-between text-lg"
+        >
+          Logout
+          <ExitIcon className="w-6 h-6" />
+        </Dropdown.Item>
+      </Dropdown.Content>
+    </Dropdown.Root>
+  );
+};
